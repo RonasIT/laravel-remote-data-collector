dd4ed02fcbd11efea81694ed89906d1a2ad39162
ameteshov <ameteshov@ronasit.com>
2017-03-03 19:15:01 +0600

    ameteshov/ add files for project

---
+++ RemoteDataCollectorServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace RonasIT\Support\RemoteDataCollector;
+
+use Illuminate\Support\ServiceProvider;
+
+class RemoteDataCollectorServiceProvider extends ServiceProvider
+{
+    public function boot() {
+        $this->publishes([
+            __DIR__.'/../config/remote-data-collector.php' => config_path('remote-data-collector.php'),
+        ], 'config');
+    }
+
+    public function register() {}
+}
---
+++ config/remote-data-collector.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'temporary_path' => env('LOCAL_DATA_COLLECTOR_TEMP_PATH'),
+    'key' => 'your-key-here',
+    'url' => 'http://example.com'
+];
